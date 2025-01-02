02b65cb53a75115923e2c773b948f0e04cbd7607
William Nolan <20107153@mail.wit.ia>
2025-01-02 21:05:24 +0000

    refactor Driver + EventController

--- demo/src/main/java/org/example/demo/EventController.java
+++ demo/src/main/java/org/example/demo/EventController.java
@@ -3,7 +3,7 @@ package org.example.demo;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class EventController {
     @FXML
     private Label welcomeText;
 
