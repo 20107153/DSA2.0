1b0419f3b3b22c29fca627d5a99d93ca2b83991f
William Nolan <20107153@mail.wit.ia>
2025-01-02 22:12:28 +0000

    made Drink.java + Ingredient.java

---
+++ demo/src/main/java/org/example/demo/models/Drink.java
@@ -0,0 +1,58 @@
+package org.example.demo.models;
+
+public class Drink<T> {
+
+    String name;
+    String placeOfOrigin;
+    String textualDescription;
+    String image;
+
+    public Drink(String name, String placeOfOrigin, String textualDescription, String image) {
+        this.name = name;
+        this.placeOfOrigin = placeOfOrigin;
+        this.textualDescription = textualDescription;
+        this.image = image;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getTextualDescription() {
+        return textualDescription;
+    }
+
+    public void setTextualDescription(String textualDescription) {
+        this.textualDescription = textualDescription;
+    }
+
+    public String getPlaceOfOrigin() {
+        return placeOfOrigin;
+    }
+
+    public void setPlaceOfOrigin(String placeOfOrigin) {
+        this.placeOfOrigin = placeOfOrigin;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Drink{" +
+                "name='" + name + '\'' +
+                ", placeOfOrigin='" + placeOfOrigin + '\'' +
+                ", textualDescription='" + textualDescription + '\'' +
+                ", image='" + image + '\'' +
+                '}';
+    }
+}
---
+++ demo/src/main/java/org/example/demo/models/Ingredient.java
@@ -0,0 +1,8 @@
+package org.example.demo.models;
+
+public class Ingredient<T> {
+
+    String name;
+    String textual_description;
+    float ABV;
+}
