7589ad7de3e448bc59a4fdabfe295699779f882d
William Nolan <20107153@mail.wit.ia>
2025-01-04 00:34:46 +0000

    fix add + remove ingredient and drinks

--- demo/src/main/java/org/example/demo/controllers/BeveragesController.java
+++ demo/src/main/java/org/example/demo/controllers/BeveragesController.java
@@ -5,31 +5,39 @@ import org.example.demo.models.Ingredient;
 
 public class BeveragesController {
     MyHashMap<String, Drink> drinksHashMap = new MyHashMap<>();
-    MyHashMap<String, Ingredient> ingredientsHashMap= new MyHashMap<>();
+    MyHashMap<String, Ingredient> ingredientsHashMap = new MyHashMap<>();
 
     /**
      * DRINKS
      */
-    public boolean addDrink(Drink drink) {
-        return drinksHashMap.put(drink);
+    public void addDrink(String name, String placeOfOrigin, String textualDescription, String image) {
+        Drink drink = new Drink(name, placeOfOrigin, textualDescription, image);
+        drinksHashMap.put(drink.getName(), drink);
     }
 
 
-    public boolean removeDrink(String drinkName){
-        if(drinksHashMap == null){
+    public void removeDrink(String drinkName) {
+        if (drinksHashMap == null) {
             System.out.println("There are no drinks to remove from the system");
-            return false;
         } else {
-            return drinksHashMap.remove(drinkName);
+            drinksHashMap.remove(drinkName);
         }
     }
-
 
 
     /**
      * INGREDIENTS
      */
-    public boolean addIngredient(String ingredient){
-        return ingredientsHashMap.put(ingredient);
+    public void addIngredient(String name, String textualDescription, int ABV) {
+        Ingredient ingredient = new Ingredient(name, textualDescription, ABV);
+        ingredientsHashMap.put(ingredient.getName(), ingredient);
+    }
+
+    public void removeIngredient(String ingredientName) {
+        if (ingredientsHashMap == null) {
+            System.out.println("There are no drinks to remove from the system");
+        } else {
+            ingredientsHashMap.remove(ingredientName);
+        }
     }
 }
