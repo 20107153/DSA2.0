d03dfaa0914febc3d83c8d9caa5e1af4de22083c
conan <20107701@mail.wit.ie>
2025-01-04 00:01:06 +0000

    add BeveragesController

--- demo/src/main/java/org/example/demo/EventController.java
+++ demo/src/main/java/org/example/demo/EventController.java
@@ -2,8 +2,10 @@ package org.example.demo;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 
 public class EventController {
-
+    @FXML
+    private TextArea drinksTextArea;
 
 }
