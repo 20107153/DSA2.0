2d28563cc8868c26c3302f0c50d4b88fe9c94d2a
William Nolan <20107153@mail.wit.ia>
2025-01-04 15:52:18 +0000

    installed xstream-1.4.21.jar, added saveDrink and loadDrink methods.

--- demo/src/main/java/org/example/demo/controllers/BeveragesController.java
+++ demo/src/main/java/org/example/demo/controllers/BeveragesController.java
@@ -1,9 +1,18 @@
 package org.example.demo.controllers;
 
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 import org.example.demo.models.Drink;
 import org.example.demo.models.Entry;
 import org.example.demo.models.Ingredient;
 import org.example.demo.models.Recipe;
+
 
 public class BeveragesController {
     MyHashMap<String, Drink> drinksHashMap = new MyHashMap<>();
@@ -26,6 +35,28 @@ public class BeveragesController {
 
     public String listDrinks(){
         return drinksHashMap.toString();
+    }
+
+    public void saveDrink() throws Exception
+    {
+        XStream xtream = new XStream(new DomDriver());
+        ObjectOutputStream out =
+                xtream.createObjectOutputStream(new FileWriter("drinks.xml"));
+        out.writeObject(drinksHashMap);
+        out.close();
+    }
+
+    public void loadDrink() throws Exception
+    {
+        Class<?>[] classes = new Class[] { Drink.class };
+
+        XStream xstream = new XStream(new DomDriver());
+        XStream.setupDefaultSecurity(xstream);
+        xstream.allowTypes(classes);
+
+        ObjectInputStream is = xstream.createObjectInputStream(new FileReader("drinks.xml"));
+        drinksHashMap = (MyHashMap<String, Drink>) is.readObject();
+        is.close();
     }
 
     /**
